8a0770bd3902707952df504aa9a7e48a282b741b
Ryan Richard <ryanmrichard1@gmail.com>
2016-07-22 16:51:57 -0400

    switched to Pulsar's pragma.h

--- LibTaskForce/Util/pragma.h
+++ LibTaskForce/Util/pragma.h
@@ -1,39 +1,40 @@
-/*  
- *   LibTaskForce: An open-source library for task-based parallelism
- * 
- *   Copyright (C) 2016 Ryan M. Richard
- * 
- *   This file is part of LibTaskForce.
- *
- *   LibTaskForce is free software: you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version.
- *
- *   LibTaskForce is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with LibTaskForce.  If not, see <http://www.gnu.org/licenses/>.
- */ 
+//File is shamelessly stolen from Pulsar
 
-/** \file pragma.h
- *  \brief Write Me!!!!!!
- *  \author Ryan M. Richard
- *  \version 1.0
- *  \date June 16, 2016
- */
-
-#ifndef LIBTASKFORCE_GUARD_PRAGMA_H
-#define LIBTASKFORCE_GUARD_PRAGMA_H
+#ifndef PULSAR_GUARD_PULSAR__PRAGMA_H_
+#define PULSAR_GUARD_PULSAR__PRAGMA_H_
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#if defined(__GNUC__)||defined(__GNUG__)
+
+
+#if defined(__ICC) || defined(__INTEL_COMPILER)
+
+    // pragmas for Intel
+    #define PRAGMA_WARNING_POP                                 _Pragma("warning(pop)")
+    #define PRAGMA_WARNING_PUSH                                _Pragma("warning(push)")
+    #define PRAGMA_WARNING_IGNORE_UNUSED_PARAMETERS            _Pragma("warning(disable:869)")
+    #define PRAGMA_WARNING_IGNORE_UNUSED_VARIABLES             _Pragma("warning(disable:177)")
+    #define PRAGMA_WARNING_IGNORE_FP_EQUALITY                  _Pragma("warning(disable:1572)")
+    #define PRAGMA_WARNING_IGNORE_FP_CONVERT                   _Pragma("warning(disable:264 173)")
+    #define PRAGMA_WARNING_IGNORE_CONVERT                      _Pragma("warning(disable:2259)")
+    #define PRAGMA_WARNING_IGNORE_SWITCH_MISSING_DEFAULT       _Pragma("warning(disable:2338)")
+    #define PRAGMA_WARNING_IGNORE_POINTLESS_COMPARISON_UINT_0  _Pragma("warning(disable:186)")
+    #define PRAGMA_WARNING_IGNORE_STATEMENT_UNREACHABLE        _Pragma("warning(disable:111)")
+    #define PRAGMA_WARNING_IGNORE_SHADOW                       _Pragma("warning(disable:1599)")
+    #define PRAGMA_WARNING_IGNORE_SHADOW_MEMBER                _Pragma("warning(disable:3280)")
+    #define PRAGMA_WARNING_IGNORE_EXTRA_SEMICOLON              // does not have warning for intel
+    #define PRAGMA_WARNING_IGNORE_REDECLARED_INLINE            _Pragma("warning(disable:522)")
+    #define PRAGMA_WARNING_IGNORE_UNUSED_LOCAL_TYPEDEFS        //! \todo add me
+    #define PRAGMA_WARNING_IGNORE_GCC_PRAGMA                   _Pragma("warning(disable:2282")
+    #define PRAGMA_WARNING_IGNORE_NONVIRTUAL_DTOR              _Pragma("warning(disable:444")
+    #define PRAGMA_WARNING_IGNORE_UNUSED_FUNCTION              //! \todo add me
+    #define PRAGMA_WARNING_IGNORE_UNRECOGNIZED_PRAGMA          _Pragma("warning(disable:161")
+
+#elif defined(__GNUC__) || defined(__GNUG__)
+
+    // pragmas for GCC
     #define PRAGMA_WARNING_PUSH                                _Pragma("GCC diagnostic push")
     #define PRAGMA_WARNING_POP                                 _Pragma("GCC diagnostic pop")
     #define PRAGMA_WARNING_IGNORE_UNUSED_PARAMETERS            _Pragma("GCC diagnostic ignored \"-Wunused-parameter\"")
@@ -54,10 +55,13 @@ extern "C" {
     #define PRAGMA_WARNING_IGNORE_UNUSED_FUNCTION              _Pragma("GCC diagnostic ignored \"-Wunused-function\"")
     #define PRAGMA_WARNING_IGNORE_UNRECOGNIZED_PRAGMA          _Pragma("GCC diagnostic ignored \"-Wunknown-pragmas\"")
 #endif
-    
+
+
+
+
+
 #ifdef __cplusplus
 }
 #endif
 
-#endif /* LIBTASKFORCE_GHUARD_PRAGMA_H */
-
+#endif
